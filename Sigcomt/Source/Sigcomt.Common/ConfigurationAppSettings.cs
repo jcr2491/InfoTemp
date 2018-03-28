c7761ef34d35d2d5942397457210ed6fdabe921b
pedro <pedroze_87@hotmail.com>
2018-03-28 12:10:49 -0500

    Update ConfigurationAppSettings.cs

--- Sigcomt/Source/Sigcomt.Common/ConfigurationAppSettings.cs
+++ Sigcomt/Source/Sigcomt.Common/ConfigurationAppSettings.cs
@@ -1,41 +1,11 @@
-﻿using System.Configuration;
+using System.Configuration;
 
 namespace Sigcomt.Common
 {
     public class ConfigurationAppSettings
     {
+        public static string ConnectionAd => ConfigurationManager.ConnectionStrings["ConnectionActiveDirectory"].ConnectionString;
+
         public static bool ValidarAd => ConfigurationManager.AppSettings.Get("ValidacionAD") == "1";
-
-        public static string CultureNameDefault()
-        {
-            var cultureNameDefault = ConfigurationManager.AppSettings.Get("CultureNameDefault");
-
-            if (string.IsNullOrEmpty(cultureNameDefault))
-            {
-                return "es-PE";
-            }
-            return cultureNameDefault;
-        }
-
-        public static string DominiosAD()
-        {
-            return ConfigurationManager.AppSettings.Get("DominiosAD");
-        }
-
-        public static string ConnectionActiveDirectory()
-        {
-            return ConfigurationManager.AppSettings.Get("ConnectionActiveDirectory");
-        }
-
-        public static int TimeOutCache()
-        {
-            var timeOutCache = ConfigurationManager.AppSettings.Get("TimeOutCache");
-
-            if (string.IsNullOrEmpty(timeOutCache))
-            {
-                return 1440;
-            }
-            return int.Parse(timeOutCache);
-        }
     }
 }
