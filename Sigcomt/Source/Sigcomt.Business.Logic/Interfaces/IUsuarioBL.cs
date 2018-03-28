089b42834aa217a9edd6b577e6db6a100d2c2753
pedro <pedroze_87@hotmail.com>
2018-03-28 12:12:37 -0500

    Update IUsuarioBL.cs

--- Sigcomt/Source/Sigcomt.Business.Logic/Interfaces/IUsuarioBL.cs
+++ Sigcomt/Source/Sigcomt.Business.Logic/Interfaces/IUsuarioBL.cs
@@ -1,9 +1,9 @@
-﻿using Sigcomt.Business.Entity;
+using Sigcomt.Business.Entity;
 
 namespace Sigcomt.Business.Logic.Interfaces
 {
     public interface IUsuarioBL
     {
-        Usuario GetUsuario(string username, string clave);
+        Usuario GetUsuario(string username);
     }
 }
